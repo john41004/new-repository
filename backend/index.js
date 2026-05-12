ffb4be9ffce39c794f7e139541918bd0f6d6a45e
john <c53311830@gmail.com>
2026-05-13 00:06:28 +0600

    update public domain

--- backend/index.js
+++ backend/index.js
@@ -150,7 +150,7 @@ app.post("/api/pdf/upload", upload.single("pdf"), async (req, res) => {
     const insertResult = await pdfCollection.insertOne(pdfDoc);
     const encodedId = encodeId(insertResult.insertedId);
 
-    const displayLink = `localhost:5173/dakhila-print/${encodedId}`;
+    const displayLink = `dakhila-ldtax-gov-bd.online/dakhila-print/${encodedId}`;
     const actualLink = `${process.env.FRONTEND_URL}/dakhila-print/${encodedId}`;
 
     const qrCodeData = await QRCode.toDataURL(displayLink);
