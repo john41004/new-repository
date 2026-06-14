daaa1d4ca5847d6b319ab17aec024fb47c9e05e6
john <c53311830@gmail.com>
2026-06-14 15:44:04 +0600

    link change

--- backend/index.js
+++ backend/index.js
@@ -133,7 +133,7 @@ const generateShortId = () => {
 
   let result = "";
 
-  for (let i = 0; i < 12; i++) {
+  for (let i = 0; i < 16; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
 
